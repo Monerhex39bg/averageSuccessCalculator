506f7b38ee0ca6a9934bd1e2ac6cdb33f5f70001
Monerhex39bg <72651606+Monerhex39bg@users.noreply.github.com>
2023-02-01 20:09:27 +0200

    Add files via upload

---
+++ averageSuccessCalculatorNew/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace averageSuccessCalculatorNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ВХОД
+            Console.Write("Напиши броят на оценките: ");
+            double numberOfGrades = double.Parse(Console.ReadLine());
+
+            //ДОПЪЛНИТЕЛНИ ПРОМЕНЛИВИ
+            double counter = 0;
+
+            //ИЗЧИСЛЕНИЯ И ИЗХОД
+            if (numberOfGrades <= 0 || numberOfGrades == 1)
+            {
+                Console.WriteLine("Въвели сте число равно или по-малко от 1!");
+            }
+            else
+            {
+                for (double i = 0; i < numberOfGrades; i++)
+                {
+                    Console.Write("Напиши оценка: ");
+                    double grade = double.Parse(Console.ReadLine());
+
+                    counter += grade;
+                }
+                double averageSuccess = counter / numberOfGrades;
+                Console.WriteLine($"Среден успех: {averageSuccess:f2}");
+            }
+        }
+    }
+}
